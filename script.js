bf118f0abe48adf053844157a835822a937503e3
Aamir Rafique <amirrafique6400@gmail.com>
2025-07-05 20:36:43 +0500

    Add responsive design and UI improvements
    Introduced a mobile-specific stylesheet (mobStyle.css) for better responsiveness. Updated index.html with a navigation bar, logo, and improved layout structure. Enhanced styles in styles.css for navigation, container, and footer. Improved script.js for better code readability and user feedback. Added logo and self-picture images.

--- script.js
+++ script.js
@@ -1,78 +1,90 @@
 
-let rockElementUser=document.getElementById('rock');
-let paperElementUser=document.getElementById('paper');
-let scissorElementUser=document.getElementById('scissor');
-let result=document.getElementById('result');
+let rockElementUser = document.getElementById('rock');
+let paperElementUser = document.getElementById('paper');
+let scissorElementUser = document.getElementById('scissor');
+let result = document.getElementById('result');
 
-let compChoice=document.getElementById('compChoice');
+let compChoice = document.getElementById('compChoice');
 
 let comp;
 let user;
 
- let scoreElementUser=document.getElementById('scoreUser');
- let scoreElementComputer=document.getElementById('scoreComputer');
+let scoreElementUser = document.getElementById('scoreUser');
+let scoreElementComputer = document.getElementById('scoreComputer');
 
- let scoreUser=0;
- let scoreComputer=0;
- 
- rockElementUser.onclick=()=>{
-     user="rock";
-     upadatingStatus();
-    }
-    paperElementUser.onclick=()=>{
-        user="paper";
-        upadatingStatus();
-    }
-    scissorElementUser.onclick=()=>{
-        user="scissor";
-        upadatingStatus();
-    }
-    
-    
-    
-    function upadatingStatus(){
-        let random=Math.floor(Math.random()*100+1);
-        if(random>=0&&random<=33)
-        comp="rock";
-    else if(random>33&&random<=66)
-    comp="paper";
-else
-{comp="scissor";}
+let scoreUser = 0;
+let scoreComputer = 0;
 
-compChoice.innerHTML=comp;
+rockElementUser.onclick = () => {
+    user = "rock";
+    upadatingStatus();
+}
+paperElementUser.onclick = () => {
+    user = "paper";
+    upadatingStatus();
+}
+scissorElementUser.onclick = () => {
+    user = "scissor";
+    upadatingStatus();
+}
 
-    if(comp==="rock"&&user==="paper"){
-        result.innerHTML="You won. Paper beats rock!";
+
+
+function upadatingStatus() {
+    let random = Math.floor(Math.random() * 100 + 1);
+    if (random >= 0 && random <= 33) {
+        comp = "rock";
+        compChoice.innerText = "👊 Rock";
+    }
+    else if (random > 33 && random <= 66) {
+        comp = "paper";
+        compChoice.innerText = "👊 Paper";
+    }
+    else {
+        comp = "scissor";
+        compChoice.innerText = "✌ Scissor";
+    }
+
+
+    if (comp === "rock" && user === "paper") {
+        result.style.backgroundColor = 'springgreen';
+        result.innerText = "You won. Paper beats rock!";
         scoreUser++;
-        scoreElementUser.innerHTML=scoreUser;
+        scoreElementUser.innerText = scoreUser;
     }
-    else if(comp==="paper"&&user==="rock"){
-        result.innerHTML="You lost. Paper beats rock!";
+    else if (comp === "paper" && user === "rock") {
+        result.style.backgroundColor = 'red';
+        result.innerText = "You lost. Paper beats rock!";
         scoreComputer++;
-        scoreElementComputer.innerHTML=scoreComputer;
+        scoreElementComputer.innerText = scoreComputer;
     }
-    else if(comp==="paper"&&user==="scissor"){
-        result.innerHTML="You won. scissor beats paper!";
+    else if (comp === "paper" && user === "scissor") {
+        result.style.backgroundColor = 'springgreen';
+        result.innerText = "You won. scissor beats paper!";
         scoreUser++;
-        scoreElementUser.innerHTML=scoreUser;
+        scoreElementUser.innerText = scoreUser;
     }
-    else if(comp==="scissor"&&user==="paper"){
-        result.innerHTML="You lost. scissor beats paper!";
+    else if (comp === "scissor" && user === "paper") {
+        result.style.backgroundColor = 'red';
+        result.innerText = "You lost. scissor beats paper!";
         scoreComputer++;
-        scoreElementComputer.innerHTML=scoreComputer;
+        scoreElementComputer.innerText = scoreComputer;
     }
-    else if(comp==="scissor"&&user==="rock"){
-        result.innerHTML="You won. rock beats scissor!";
-        scoreElementUser.innerHTML=scoreUser;
+    else if (comp === "scissor" && user === "rock") {
+        result.style.backgroundColor = 'springgreen';
+        result.innerText = "You won. rock beats scissor!";
+        scoreElementUser.innerText = scoreUser;
         scoreUser++;
     }
-    else if(comp==="rock"&&user==="scissor"){
-        result.innerHTML="You lost. rock beats scissor!";
+    else if (comp === "rock" && user === "scissor") {
+        result.style.backgroundColor = 'red';
+        result.innerText = "You lost. rock beats scissor!";
         scoreComputer++;
-        scoreElementComputer.innerHTML=scoreComputer;
+        scoreElementComputer.innerText = scoreComputer;
     }
-    else{
-        result.innerHTML="It was a Draw";
+    else {
+        result.innerText = "It was a Draw";
+        result.style.backgroundColor='darkblue  ';
     }
 }
 
